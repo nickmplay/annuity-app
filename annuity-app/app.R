f28896da91157dd9bc11bb78a4d5b31c1c1d9162
Nick M <nickmontagni@hotmail.com>
2023-02-14 15:17:21 +0000

    iris plot

--- annuity-app/app.R
+++ annuity-app/app.R
@@ -1,47 +1,47 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
 
-# Define UI for application that draws a histogram
+# Define UI for application that draws a plot
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Iris petal data"),
 
-    # Sidebar with a slider input for number of bins 
+    # Sidebar with a slider input for iris species
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            selectInput("species_selection", "Species", 
+                        multiple = T,
+                        choices = c("setosa", "versicolor", "virginica"))
         ),
 
-        # Show a plot of the generated distribution
+        # Show a plot of the iris species by petal length and width
         mainPanel(
            plotOutput("distPlot")
         )
     )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic required to draw a plot
 server <- function(input, output) {
 
     output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+      # generate empty plot
+      plot(1, type = "n", 
+           xlab = "Petal Length", ylab = "Petal Width", 
+           xlim = c(0, 7), ylim = c(0, 3))
+      
+      # define colour mapping
+      iris_cols <- c("setosa" = 1, "versicolor" = 2, "virginica" = 3)
+      
+      # loop selection and add points to plot
+      for(i in input$species_selection){
+        points(iris[iris$Species == i, "Petal.Length"], 
+               iris[iris$Species == i, "Petal.Width"], 
+               col = iris_cols[[i]], 
+               pch = 16)
+      }
+      
     })
 }
 
