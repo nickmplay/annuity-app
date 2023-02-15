0ae5c709907e5769de7818ed33ff10596afa85c2
Nick M <nickmontagni@hotmail.com>
2023-02-15 11:02:43 +0000

    three reinsurers

--- annuity-app/app.R
+++ annuity-app/app.R
@@ -1,11 +1,23 @@
 
+
+# load dependencies -------------------------------------------------------
+
 library(shiny)
 
-# load prepared data
-df = read.csv("annuities.csv")
-colnames(df)[1] <- gsub('^...','',colnames(df)[1])
 
-# Define UI for application that draws a plot
+# define global variables -------------------------------------------------
+
+# global data
+df = read.csv("annuities.csv")
+
+# define colour mapping
+df_cols <- c("McDonald Re" = "#C77CFF", 
+             "RGB" = "#F8766D", 
+             "MetDeath" = "#00BFC4")
+
+
+# Define UI for application that draws a plot -----------------------------
+
 ui <- fluidPage(
 
     # Application title
@@ -14,9 +26,23 @@ ui <- fluidPage(
     # Sidebar with a slider input for iris species
     sidebarLayout(
         sidebarPanel(
-            selectInput("ri_selection", "Reinsurer", 
+          selectInput("deal_selection", "Deal", 
+                      choices = unique(df$Deal)),
+                      
+          selectInput("ri_selection", "Reinsurer", 
                         multiple = T,
-                        choices = unique(df$Reinsurer))
+                        choices =  unique(df$Reinsurer)),
+          
+          sliderInput(
+            "age_selection", label = "Age range",
+            min = 50, value = c(50, 90), max = 90
+          ), 
+          
+          fluidRow(p(verbatimTextOutput("ri_winner"))),
+          
+          # debug
+          fluidRow(column(12, verbatimTextOutput("ri_select"))),
+          
         ),
 
         # Show a plot of the iris species by petal length and width
@@ -27,49 +53,78 @@ ui <- fluidPage(
     )
 )
 
-# Define server logic required to draw a plot
-server <- function(input, output) {
 
+# Define server logic required to draw a plot -----------------------------
+
+server <- function(input, output) {
+  
+    # select reinsurers from selected deal
+    output$ri_select <- renderPrint({
+      dput(unique(df[df$Deal == input$deal_selection, "Reinsurer"]))
+    })
+  
+    # render male plot
     output$distPlot1 <- renderPlot({
       # generate empty plot
       plot(1, type = "n", 
            main = "Male annuities",
            xlab = "Age", ylab = "% BEL", 
-           xlim = c(50, 80), ylim = c(-0.02, 0.09))
-      
-      # define colour mapping
-      df_cols <- c("R1" = 1, "R2" = 2)
+           xlim = c(50, 90), ylim = c(-0.02, 0.09))
+
       
       # loop selection and add points to plot
-      df_s <- df[df$Sex == "Male", ]
+      age_range <- input$age_selection[1]:input$age_selection[2]
+      df_s <- df[df$Sex == "Male" & 
+                   df$Deal == input$deal_selection & 
+                   df$Age %in% age_range, ]
       for(i in input$ri_selection){
         lines(df_s[df_s$Reinsurer == i, "Age"], 
                df_s[df_s$Reinsurer == i, "Value"], 
-               col = df_cols[[i]], 
+               col = df_cols[[i]], lwd = 2,
                pch = 16)
       }
-    }, height = 450, width = 700)
+    })
     
+    # render female plot
     output$distPlot2 <- renderPlot({
       # generate empty plot
       plot(1, type = "n", 
            main = "Female annuities",
            xlab = "Age", ylab = "% BEL", 
-           xlim = c(50, 80), ylim = c(-0.02, 0.09))
-      
-      # define colour mapping
-      df_cols <- c("R1" = 1, "R2" = 2)
+           xlim = c(50, 90), ylim = c(-0.02, 0.09))
+    
       
       # loop selection and add points to plot
-      df_s <- df[df$Sex == "Female", ]
+      age_range <- input$age_selection[1]:input$age_selection[2]
+      df_s <- df[df$Sex == "Female" & 
+                   df$Deal == input$deal_selection & 
+                   df$Age %in% age_range, ]
       for(i in input$ri_selection){
         lines(df_s[df_s$Reinsurer == i, "Age"], 
                df_s[df_s$Reinsurer == i, "Value"], 
-               col = df_cols[[i]], 
+               col = df_cols[[i]], lwd = 2,
                pch = 15)
       }
-    }, height = 450, width = 700)
+    })
+    
+    # winner 
+    output$ri_winner <- renderPrint({
+      age_range <- input$age_selection[1]:input$age_selection[2]
+      ri_rank <- aggregate(Value ~ Reinsurer, 
+                           df[df$Deal == input$deal_selection & 
+                                df$Age %in% age_range, ], 
+                           sum)
+      paste("The lead reinsurer is", 
+            ri_rank[ri_rank$Value == min(ri_rank$Value), "Reinsurer"])
+    })
+    
+    # debugging
+    output$debug <- renderPrint({ input$age_selection })
+    
+    
 }
 
-# Run the application 
+
+# Run the application  ----------------------------------------------------
+
 shinyApp(ui = ui, server = server)
